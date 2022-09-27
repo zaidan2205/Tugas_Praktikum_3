7344c207afefe8fba93a633619b6256234790e6a
zaidan2205 <72377693+zaidan2205@users.noreply.github.com>
2022-09-27 22:45:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktikum3/ResetPassword.java
@@ -0,0 +1,15 @@
+package com.example.praktikum3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
